729126be2018ff4800ec2d73afbc2e591252d88d
Petras Slekys <Petras.Slekys@ibm.com>
2020-07-29 20:01:15 +0300

    init

---
+++ src/services/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAwgTSRcP4Qb-JHPS2DQmiTIsmT7o0LlPU",
+  authDomain: "ca-test-b41bb.firebaseapp.com",
+  databaseURL: "https://ca-test-b41bb.firebaseio.com",
+  projectId: "ca-test-b41bb",
+  storageBucket: "ca-test-b41bb.appspot.com",
+  messagingSenderId: "336299526949",
+  appId: "1:336299526949:web:530cdb467aa3837e1428cc",
+};
+
+firebase.initializeApp(firebaseConfig);
